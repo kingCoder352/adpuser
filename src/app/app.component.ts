c23b95c45e8ce7c2144197884feb1369571433eb
Savage Coder <savagecoder352@gmail.com>
2022-04-22 02:47:59 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AppService} from "./app.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy  {
+  constructor(
+    public appService: AppService
+  ) {
+  }
+
   title = 'adpuser';
+  allUsers: any;
+  users: any;
+
+  ngOnInit() {
+    this.appService.getUserData().subscribe(data => {
+      this.users = data;
+      this.allUsers = this.users;
+    });
+  }
+
+  searchForUser(event: any) {
+    let searchValue = event.target.value;
+    this.users = this.allUsers.filter((val: any) => {
+      return val.name.toLowerCase().includes(searchValue.toLowerCase());
+    });
+  }
+
+  ngOnDestroy() {
+
+  }
 }
